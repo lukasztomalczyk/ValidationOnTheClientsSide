c6fab2fe95f8fd07c759fc32a485574ed13c484b
Unknown <lukasz.tomalczyk@outlook.com>
2018-03-14 16:36:00 +0100

    Initializing Repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationClientSite.Models;
+
+namespace ValidationClientSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Person _person)
+        {
+            return (ModelState.IsValid) ?  View("Index", _person) : View();
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ValidationClientSite.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Name")]
+        [StringLength(25, ErrorMessage = "the name can not exceed 25 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Surname is required")]
+        [Display(Name = "Surname")]
+        [StringLength(25, ErrorMessage = "the surname can not exceed 25 characters")]
+        public string SecondName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Not a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Display(Name = "PhoneNumber")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Entered phone format is not valid.")]
+        public string NumberPhone { get; set; }
+    }
+}
